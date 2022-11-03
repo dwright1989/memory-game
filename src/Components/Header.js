8b92f8724f4e715479e54437a0848106baf40fcf
dwright1989 <debbiewright@live.co.uk>
2022-11-03 08:12:04 +0000

    CSS changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,12 +8,15 @@ export default function Header(props) {
             <div id="header">
                 <h1>Memory Game</h1>
                 <p>Try to click all the cards without clicking the same one twice!</p>
-                <div>
-                    Current Score: {currentScore}
+                <div id="score">
+                    <p id="currentScore">
+                        Current Score: {currentScore}
+                    </p>
+                    <p id="bestScore">
+                        Best Score: {bestScore}
+                    </p>
                 </div>
-                <div>
-                    Best Score: {bestScore}
-                </div>
+                
             </div>
         );
 }
